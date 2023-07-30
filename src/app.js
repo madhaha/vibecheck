b25b07421f7548d05ce9b608ba3533a4193caf2b
madhaha <98023878+madhaha@users.noreply.github.com>
2023-07-30 16:20:45 +0000

    try quering group info

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-console.log('Document updated:', (new Date).toISOString(), process.env.USERNAME);
+const vrchat = require('vrchat');
+const totp = require('totp-generator');
+
+const configuration = new vrchat.Configuration({
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD
+});
+
+const AuthenticationApi = new vrchat.AuthenticationApi(configuration);
+const token = totp(process.env.TOTP);
+AuthenticationApi.verify2FA(token).then(() => {
+    const GroupsApi = new vrchat.GroupsApi(configuration);
+    GroupsApi.getGroupMembers(process.env.GRP_ID).then(res => {
+        console.log('Document updated:', (new Date).toISOString(), res);
+    })
+});
